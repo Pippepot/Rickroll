ee491890e8e3974530ef6ec046a61c12f09ffe47
Pippepot <44341689+Pippepot@users.noreply.github.com>
2021-12-27 13:45:29 +0100

    InstallRickRoll.bat no longer opens internet explorer

--- InstallRickRoll.bat
+++ InstallRickRoll.bat
@@ -2,7 +2,3 @@ cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
 InstallUtil.exe %~dp0RickRoll.exe
 sc config RicksService start=auto
 net start AstleyLovesYou
-exit
-Start "" "%ProgramFiles%\Internet Explorer\iexplore.exe" "www.google.com"
-Timeout 50
-Taskkill /IM "iexplore.exe" /F
